9ae35c48f952276c60837379a278f2f3e070065b
Yimin ZHOU <55327029+Yimin-zhou@users.noreply.github.com>
2023-06-27 19:10:25 +0200

    remove egcn

--- evolveGCN/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .gcn import GCNSequential
-from .gcn_skip import GCNTwoLayersSkipConnection
-from .evolve_gcn import EvolveGCN
--- evolveGCN/models/layers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .gcn_layer import GCNLayer
-from .gcn_skip_layer import GCNSkipLayer
-from .h_gru import HGRUCell
-from .summarize import SummarizeLayer
-from .e_gcu_h import EGCUH
